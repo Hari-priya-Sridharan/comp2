67dc6c6369942c96776e4efb2d3f384b9c1e6075
Hari-priya-Sridharan <65731962+Hari-priya-Sridharan@users.noreply.github.com>
2022-09-21 13:53:27 +0530

    Prometheus is added grafana is yet to be added

--- src/main/java/com/tweetApp/comp2/Config/Security/WebSecurityConfiguration.java
+++ src/main/java/com/tweetApp/comp2/Config/Security/WebSecurityConfiguration.java
@@ -12,6 +12,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers("/actuator").permitAll()
+                .antMatchers("/actuator/prometheus").permitAll()
                 .antMatchers(HttpMethod.POST,"/api/v1.0/tweets/register").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1.0/tweets/login").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1.0/tweets/{username}/forgot").permitAll()
