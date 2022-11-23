749f49d7494cb4a0c39ad7753684f1a3a8d9f757
Hari-priya-Sridharan <65731962+Hari-priya-Sridharan@users.noreply.github.com>
2022-11-24 00:20:44 +0530

    AWS deployment changes

--- src/main/java/com/tweetApp/comp2/Comp2Application.java
+++ src/main/java/com/tweetApp/comp2/Comp2Application.java
@@ -2,7 +2,9 @@ package com.tweetApp.comp2;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
